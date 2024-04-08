d37e9956e852404627d420be9436cfd0860a07eb
RMLemos <143395332+RMLemos@users.noreply.github.com>
2024-04-08 13:59:43 +0100

    Models

---
+++ XiaBags_Ecommerce/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace XiaBags_Ecommerce.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public string CategoryDescription { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        [DefaultValue(typeof(DateTime), "")]
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
---
+++ XiaBags_Ecommerce/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace XiaBags_Ecommerce.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        public string ImageThumbnailURL { get; set; }
+
+        public bool StockInHand { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        [DefaultValue(typeof(DateTime), "")]
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
